5de645541b6dc935baf5c560e00285e5c0d6ca45
Vincent Marinilli <marinilliv@gmail.com>
2023-07-22 18:29:10 -0400

    Initial Code And Tests

--- App/Classes.py
+++ App/Classes.py
@@ -1,1 +1,25 @@
+class MenuItem:
+    def __init__(self, name):
+        self.name = name
 
+    def __str__(self):
+        return self.name
+    
+class Review:
+    def __init__(self, menu_item, review_text, sentiment_score):
+        self.menu_item = menu_item
+        self.review_text = review_text
+        self.sentiment_score = sentiment_score
+
+class ReviewDatabase:
+    def __init__(self):
+        self.reviews = []
+
+    def add_review(self, review):
+        self.reviews.append(review)
+
+    def get_reviews_by_menu_item(self, menu_item_name):
+        return [review for review in self.reviews if review.menu_item.name == menu_item_name]
+
+    def __str__(self):
+        return "\n".join(str(review) for review in self.reviews)
--- Test/Class_Test.py
+++ Test/Class_Test.py
@@ -1,1 +1,15 @@
+from app.Classes import MenuItem, Review, ReviewDatabase
+import pytest
 
+def test_menu_item_creation(self):
+    menu_item = MenuItem("Burger")
+    self.assertEqual(menu_item.name, "Burger")
+
+
+def test_review_creation(self):
+    menu_item = MenuItem("Pizza")
+    review = Review(menu_item, "The pizza was delicious!", 0.8)
+
+    self.assertEqual(review.menu_item, menu_item)
+    self.assertEqual(review.review_text, "The pizza was delicious!")
+    self.assertEqual(review.sentiment_score, 0.8)
